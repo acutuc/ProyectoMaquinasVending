b455c35bfe8334693e2059f1e57ccc81999b8ee4
acutuc <acutuc94@gmail.com>
2022-01-18 14:44:29 +0100

    Actualización de clase. Nueva clase Productos agregada. Revisar si se puede implantar directamente como atributo de Bandejas().

--- src/main/java/daw/poo/maquinavending/gabrielallendejuanjosebazan/ModoUsuario.java
+++ src/main/java/daw/poo/maquinavending/gabrielallendejuanjosebazan/ModoUsuario.java
@@ -5,5 +5,11 @@ package daw.poo.maquinavending.gabrielallendejuanjosebazan;
  * @author Gabriel
  */
 public class ModoUsuario {
+    private Productos producto1;
+    private Productos producto2;
+    private Productos producto3;
+    private Productos producto4;
+    private Productos producto5;
+    private Productos producto6;
     
 }
