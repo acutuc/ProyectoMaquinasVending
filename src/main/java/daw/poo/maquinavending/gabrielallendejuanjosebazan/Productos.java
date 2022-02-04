573de17aaf35b21d3e6e149c249a5063375a339a
acutuc <acutuc94@gmail.com>
2022-02-04 21:53:58 +0100

    Eliminado atributo stock

--- src/main/java/daw/poo/maquinavending/gabrielallendejuanjosebazan/Productos.java
+++ src/main/java/daw/poo/maquinavending/gabrielallendejuanjosebazan/Productos.java
@@ -11,17 +11,16 @@ public class Productos {
     //Atributos de la clase Productos:
     private String nombreProducto;
     private int precioProducto; //Precio de producto en CÉNTIMOS.
-    private int stockProducto; //Stock del producto. Decrecerá al vender o aumentará al rellenar.
 
     //Constructor parametrizado.
-    public Productos(String nombreProducto, int precioProducto, int stockProducto) {
+    public Productos(String nombreProducto, int precioProducto) {
         this.nombreProducto = nombreProducto;
         this.precioProducto = precioProducto;
         //Restricción que controla que el precio de un producto nunca pueda ser negativo.
         if(precioProducto < 0){
             this.precioProducto = Math.abs(precioProducto);
         }
-        this.stockProducto = stockProducto;
+        
     }
 
     //Constructor con sus valores predeterminados.
@@ -49,29 +48,12 @@ public class Productos {
         this.precioProducto = precioProducto;
     }
 
-    public int getStockProducto() {
-        return stockProducto;
-    }
-
-    public void setStockProducto(int stockProducto) {
-        this.stockProducto = stockProducto;
-    }
-    
-    
+        
     //toString.
 
     @Override
     public String toString() {
         return "Productos{" + "nombreProducto=" + nombreProducto + ", precioProducto=" + precioProducto + '}';
     }
-       
-    public void rellenarProducto(int cantidadProductosAMeter){
-        this.stockProducto = stockProducto + cantidadProductosAMeter;
-    }
-   
-    public void productoVendido(){
-        this.stockProducto = stockProducto - 1;
-    }
-    
-    
+        
 }
